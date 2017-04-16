a7740ab9754a3167749555df4e05392245350778
Tom Schenk Jr <tomschenkjr@gmail.com>
2017-04-16 23:29:28 +0000

    Migrating from Google Drive

---
+++ README.md
@@ -0,0 +1,38 @@
+Introduction
+============
+This project contains the source files for the [LaTeX for Undergraduates] tutorial. The tutorial is designed for teaching undergraduate students how to use the LaTeX typesetting program.  The emphasis of this tutorial is to (1) teach the basic of LaTeX; (2) introduce advanced concepts that students may need to use; (3) show how laTeX works with third-party programs (such as Mathematica); (4) provide several references to other literature to entice students to continue learning LaTeX. This repository contains all of the source material used to create the tutorial, permitting the tutorial to be used and adapted.
+
+Organization of Tutorial
+------------------------
+This tutorial mainly uses slides designed for classroom lectures, however, they are detailed enough so they can be understood outside the classroom. The tutorial is broken down into section and subsections. Each slide show is about 5 to 20 minutes long, so two or three can be presented in a typical class.
+
+File Structure
+==============
+There are several folders with files, though, some are incomplete. Below is a brief description of the contents of each.
+
+Handouts (Incomplete)
+---------------------
+Contains a single handout given to participants in the the LaTeX for Undergraduates tutorial at Drake University in 2005. Contains LaTeX and PDF files.
+
+Lecture Notes (Incomplete)
+--------------------------
+Written documentation intended to accompany the tutorial slides. 
+
+Lecture Recordings (Incomplete)
+-------------------------------
+Recordings of the first lecture given to the LaTeX for Undergraduates tutorial at Drake University during the fall 2005. Contains AUP files that can be opened with [Audacity].
+
+Slides (Complete)
+-----------------
+Contains several sub-folders with slides used in the tutorial. Files include LaTeX files, postscript, PDFs, and, when applicable, encapsulated postscript files for images. Each folder contains individual folders for each section and subsection.
+
+Website (Complete)
+------------------
+Contains relevant files for the original LaTeX for Undergraduates webpage. It has minimal applicability to the current project, but may serve as a template for other sites.
+
+License
+=======
+This license supercedes any other license mentioned within these files. This document is copyrighted by Tom Schenk Jr.. It may be modified and re-compiled for non-commercial use. Subsequent authors modifying this document for LaTeX for Undergraduates may append their name(s) in the ``\author'' command. They may not delete the names of previous authors.
+
+[LaTeX for Undergraduates]: http://tomschenkjr.net/latex-tutorial/
+[Audacity]: http://audacity.sourceforge.net/
---
+++ Website/toc.mm
@@ -0,0 +1,125 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1146708339234" ID="Freemind_Link_1840463010" LINK="http://latex.genericface.com" MODIFIED="1146710407875" TEXT="LaTeX for Undergraduates">
+<font NAME="SansSerif" SIZE="20"/>
+<icon BUILTIN="gohome"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1146708361843" ID="_" LINK="http://www.drake.edu" MODIFIED="1146710309421" POSITION="left" TEXT="Drake University" VGAP="7" VSHIFT="-23">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1146708371453" ID="Freemind_Link_1628792388" LINK="http://www.drake.edu/artsci/mathcs/" MODIFIED="1146709346484" TEXT="Department of Mathematics &amp; Computer Science">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1146708422421" ID="Freemind_Link_1189259237" LINK="http://lists.drake.edu/index.php?blog=7" MODIFIED="1146709346500" TEXT="LaTeX for Undergraduates&apos; Dev Blog">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1146708383484" ID="Freemind_Link_57002730" LINK="http://www.genericface.com" MODIFIED="1146710255250" POSITION="left" TEXT="genericface.com" VSHIFT="-5">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1146708401625" ID="Freemind_Link_384987677" MODIFIED="1146710232531" POSITION="right" TEXT="Introduction" VSHIFT="-1">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1146708439234" ID="Freemind_Link_1204690251" LINK="tutorial/introduction/intro/" MODIFIED="1146709346515" TEXT="Introduction to LaTeX">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1146708446531" ID="Freemind_Link_1514957105" LINK="tutorial/introduction/how/" MODIFIED="1146709346515" TEXT="How LaTeX Works">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1146708456828" ID="Freemind_Link_1860994597" MODIFIED="1146709346531" TEXT="Installing LaTeX">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1146708462578" ID="Freemind_Link_853366345" LINK="tutorial/introduction/install/win/" MODIFIED="1146709346531" TEXT="Windows">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1146708469875" ID="Freemind_Link_1805505145" LINK="tutorial/introduction/install/mac/" MODIFIED="1146709346531" TEXT="MacOS">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1146708479703" ID="Freemind_Link_161786551" MODIFIED="1146710225812" POSITION="right" TEXT="Creating Documents" VSHIFT="10">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1146708488453" ID="Freemind_Link_1198292243" MODIFIED="1146709346546" TEXT="Preamble">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1146708542125" ID="Freemind_Link_1307244419" LINK="tutorial/create/preamble/classpack/" MODIFIED="1146709346546" TEXT="Classes and Packages">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1146708546843" ID="Freemind_Link_296941208" LINK="tutorial/create/preamble/title/" MODIFIED="1146709346562" TEXT="Title, Author, and Layout">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1146708489765" ID="Freemind_Link_1006569717" MODIFIED="1146709346562" TEXT="Body">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1146708519859" ID="Freemind_Link_1787919355" LINK="tutorial/create/body/sect/" MODIFIED="1146709346578" TEXT="Linebreaks, Paragraphs, and Sections">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1146708536125" ID="Freemind_Link_1024932419" LINK="tutorial/create/body/format/" MODIFIED="1146709346578" TEXT="Formatting">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1146708499906" ID="Freemind_Link_1064577528" MODIFIED="1146709346578" TEXT="Generating Documents">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1146708507609" ID="Freemind_Link_689075876" LINK="tutorial/create/generate/win/" MODIFIED="1146709346593" TEXT="Windows">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1146708512062" ID="Freemind_Link_1027797822" LINK="tutorial/create/generate/mac/" MODIFIED="1146709346593" TEXT="MacOS">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1146708555718" ID="Freemind_Link_561149912" MODIFIED="1146710222078" POSITION="right" TEXT="Intermediate Documents" VSHIFT="11">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1146708562187" ID="Freemind_Link_1861142736" MODIFIED="1146709346593" TEXT="Environments">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1146708573453" ID="Freemind_Link_1565285143" LINK="tutorial/intermediate/environments/genenv/" MODIFIED="1146709346593" TEXT="Environments">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1146708576250" ID="Freemind_Link_475773758" LINK="tutorial/intermediate/environments/mathenv/" MODIFIED="1146709346609" TEXT="Math Environments">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1146708579687" ID="Freemind_Link_1707869442" LINK="tutorial/intermediate/environments/table/" MODIFIED="1146709346609" TEXT="Tables">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1146708565687" ID="Freemind_Link_91952326" MODIFIED="1146709346609" TEXT="Indexing and Bibliographies">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1146708582203" ID="Freemind_Link_1454902921" LINK="tutorial/intermediate/indexing/toc/" MODIFIED="1146709346625" TEXT="Table of Contents">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1146708587375" ID="Freemind_Link_1668188409" LINK="tutorial/intermediate/indexing/biblio/" MODIFIED="1146709346625" TEXT="Bibliographies">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1146708589968" ID="Freemind_Link_1742542413" LINK="tutorial/intermediate/indexing/index/" MODIFIED="1146709346625" TEXT="Index">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1146709188296" ID="Freemind_Link_457508891" MODIFIED="1146710244546" POSITION="right" TEXT="Advanced Topics" VSHIFT="6">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1146709206703" ID="Freemind_Link_363146158" MODIFIED="1146709346640" TEXT="Floating">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1146709225703" ID="Freemind_Link_1036808675" MODIFIED="1146709346640" TEXT="Tabular and Figures">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1146709211671" ID="Freemind_Link_1461320216" MODIFIED="1146709346640" TEXT="Advanced Mathematical Documents">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1146709219078" ID="Freemind_Link_690597545" LINK="tutorial/advanced/math/theorem/" MODIFIED="1146709346640" TEXT="Theorem Environment">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1146708597343" ID="Freemind_Link_1605485929" MODIFIED="1146710242656" POSITION="right" TEXT="Appendix" VSHIFT="15">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1146708630406" ID="Freemind_Link_1481640831" MODIFIED="1146709346656" TEXT="3rd party Programs &amp; LaTeX">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1146708639875" ID="Freemind_Link_1914635969" MODIFIED="1146709346656" TEXT="Mathematica">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1146708644015" ID="Freemind_Link_1348975417" LINK="tutorial/appendix/r/" MODIFIED="1146709346671" TEXT="GNU R">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1146708646656" ID="Freemind_Link_985649537" LINK="tutorial/appendix/gnuplot/" MODIFIED="1146709346671" TEXT="gnuplot">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
